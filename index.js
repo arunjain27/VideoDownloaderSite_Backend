6b15cf60a911771c705176f2a6428b43bb1bf8d7
arunjain27 <arunjain2704@gmail.com>
2026-02-07 16:21:50 +0530

    Update Backend BUgs

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ mongoose.connect(process.env.BASE_URL, {
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
+/*
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+*/
